dcb2f6496227d22fb6b971b9025b19b2411f62ba
Victor Soffi <victorsoffi@gmail.com>
2024-08-14 12:35:07 -0300

    import

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../../componentes/Navbar';
 import Footer from '../../componentes/Footer';
-import styles from '../Home/Home.module.css'
+import styles from './Home.module.css'
 
 const Home = () => {
   return (
