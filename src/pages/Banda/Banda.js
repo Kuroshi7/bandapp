7d397c5efda2342d25a1a2452e3106e83ba5d779
Victor Soffi <victorsoffi@gmail.com>
2024-08-15 12:01:41 -0300

    CSS+Banda.js

--- src/pages/Banda/Banda.js
+++ src/pages/Banda/Banda.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Navbar from '../../componentes/Navbar';
+import styles from './Banda.module.css';
+import Footer from '../../componentes/Footer';
 
 const Banda = () => {
   return (
-    <div>
+    <div className={styles.bandaContainer}>
         <Navbar/>
+    <div className={styles.contentBox}>
       <h1>Banda</h1>
       <p>Informações sobre a banda.</p>
+    </div>
+    <Footer/>
     </div>
   );
 };
