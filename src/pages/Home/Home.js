7536819b67160c9a82980ff8318b5fb505e7e600
Victor Soffi <victorsoffi@gmail.com>
2024-09-01 22:01:59 -0300

    iframeYT

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,6 +10,17 @@ const Home = () => {
       <div className={styles.contentBox}>
         <h1>Bem-vindo à Home Page!</h1>
         <p>Este é o conteúdo principal da página.</p>
+        <div className={styles.youtubeEmbed}>
+        <iframe width="100%" 
+        height="500"
+        src="https://www.youtube.com/embed/yI1sn4i2aEI?si=tTPCH7tTuqMZDII7" 
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+        ></iframe>
+        </div>
       </div>
       <Footer />
     </div>
