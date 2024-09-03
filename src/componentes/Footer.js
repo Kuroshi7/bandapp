3886c6cfb4d35fbe2695101b0814632520010efd
Victor Soffi <victorsoffi@gmail.com>
2024-09-03 10:22:26 -0300

    comments

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.socialButtons}>
-        {/* Adicione seus botões sociais aqui, como ícones do FontAwesome */}
+        {/* botões socials aqui FontAwesome */}
         <button className={styles.socialButton}>Instagram</button>
         <button className={styles.socialButton}>YouTube</button>
         <button className={styles.socialButton}>Spotify</button>
